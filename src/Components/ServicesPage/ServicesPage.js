7004ad30d8e3bfe26e3216340f17d0285253fc90
anitha0203 <anithaboppidi@gmail.com>
2024-02-08 17:36:07 +0530

    08-02-24

---
+++ src/Components/ServicesPage/ServicesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ServicesPage.css'
+
+function ServicesPage() {
+  return (
+    <div>ServicesPage</div>
+  )
+}
+
+export default ServicesPage
---
+++ src/Components/WorkPage/WorkPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './WorkPage.css'
+
+function WorkPage() {
+  return (
+    <div>WorkPage</div>
+  )
+}
+
+export default WorkPage
