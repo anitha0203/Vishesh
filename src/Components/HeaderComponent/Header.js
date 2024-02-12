6934e7749d1374d1c1d1c8cf16f52757c4ec37fd
anitha0203 <anithaboppidi@gmail.com>
2024-02-12 11:24:29 +0530

    header

--- src/Components/HeaderComponent/Header.js
+++ src/Components/HeaderComponent/Header.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './Header.css'
 import Logo from '../../assets/logo.png'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -23,6 +24,11 @@ function Header() {
     };
   }, []);
 
+  const isActive = (path) => {
+    // Check if the current path matches the provided path
+    return location.pathname === path;
+  };
+
   return (
     <div>
       <div className='main-heading'>
@@ -40,11 +46,11 @@ function Header() {
 
       <div className='header-section'>
         <div className='nav-items-section'>
-          <h2 className='nav-items' onClick={() => { navigate('/home') }}>Home</h2>
-          <h2 className='nav-items' onClick={() => { navigate('/services') }}>Services</h2>
-          <h2 className='nav-items' onClick={() => { navigate('/work') }}>Work</h2>
-          <h2 className='nav-items' onClick={() => { navigate('/about') }}>About</h2>
-          <h2 className='nav-items' onClick={() => { navigate('/contact') }}>Contact</h2>
+          <h2 className={`nav-items ${isActive('/') || isActive('/home') ? 'active' : ''}`} onClick={() => { navigate('/home') }}>Home</h2>
+          <h2 className={`nav-items ${isActive('/services') ? 'active' : ''}`} onClick={() => { navigate('/services') }}>Services</h2>
+          <h2 className={`nav-items ${isActive('/work') ? 'active' : ''}`} onClick={() => { navigate('/work') }}>Work</h2>
+          <h2 className={`nav-items ${isActive('/about') ? 'active' : ''}`} onClick={() => { navigate('/about') }}>About</h2>
+          <h2 className={`nav-items ${isActive('/contact') ? 'active' : ''}`} onClick={() => { navigate('/contact') }}>Contact</h2>
         </div>
       </div>
     </div>
