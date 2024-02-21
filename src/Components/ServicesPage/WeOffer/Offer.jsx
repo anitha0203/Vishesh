6af4eaddb36f7ba173fed76954d3cb3504988565
vinodkumarmaradana2514@gmail.com <vinodkumarmaradana2514@gmail.com>
2024-02-21 16:03:20 +0530

    master

--- src/Components/ServicesPage/WeOffer/Offer.jsx
+++ src/Components/ServicesPage/WeOffer/Offer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container,Row,Col,Image } from "react-bootstrap"; // Import Container from react-bootstrap
+import { Container, Row, Col, Image } from "react-bootstrap"; // Import Container from react-bootstrap
 import "./Offer.css";
 // import creative from "../../../assets/ServicePage/creative1.png";
 // import ui from "../../../assets/ServicePage/ui.png";
@@ -52,95 +52,95 @@ function Offer() {
             <div className="TextToggle-Inside1">
                 {selectedText === "Text 1" && (
                     <div className="specific-div">
-                       <Image src='' width={'880px'} style={{borderRadius:'24px'}}/>
-                       <Row>
-                     
-                        <Col className="col-offer"><p>Logo Design</p>
-                        <p>Video Creation & Editing</p>
-                        <p>Business Card Design</p>
-                        <p>Packaging Design</p>
-                        <p>Branding and Identity Design</p>
-                        <p>Infographics</p></Col>
-                        <Col className="col-offer"><p>Banner and Signage Design</p>
-                        <p>Display Advertising</p>
-                        <p>Product Labelling</p>
-                        <p>Social Media Post and Ad Design</p>
-                        <p>Print Design (Brochures & Flyers) /</p>
-                        <p>Social Media Posters /</p></Col>
-                       </Row>
+                        <Image src='' width={'880px'} style={{ borderRadius: '24px' }} />
+                        <Row className="TextToggle-row">
+
+                            <Col className="col-offer"><p>Logo Design</p>
+                                <p>Video Creation & Editing</p>
+                                <p>Business Card Design</p>
+                                <p>Packaging Design</p>
+                                <p>Branding and Identity Design</p>
+                                <p>Infographics</p></Col>
+                            <Col className="col-offer"><p>Banner and Signage Design</p>
+                                <p>Display Advertising</p>
+                                <p>Product Labelling</p>
+                                <p>Social Media Post and Ad Design</p>
+                                <p>Print Design (Brochures & Flyers) /</p>
+                                <p>Social Media Posters /</p></Col>
+                        </Row>
                     </div>
                 )}
                 {selectedText === "Text 2" && (
-                   <div className="specific-div">
-                   <Image src='' width={'880px'} style={{borderRadius:'24px'}}/>
-                   <Row>
-                 
-                    <Col className="col-offer"><p>User Research</p>
-                    <p>User Experience (UX) Design</p>
-                    <p>User Interface (UI) Design</p>
-                  </Col>
-                    <Col className="col-offer"><p>Prototyping and Testing</p>
-                    <p>Web UX/UI Design</p>
-                    <p>Mobile App UX/UI Design</p>
-                   </Col>
-                   </Row>
-                </div>
+                    <div className="specific-div">
+                        <Image src='' width={'880px'} style={{ borderRadius: '24px' }} />
+                        <Row className="TextToggle-row">
+
+                            <Col className="col-offer"><p>User Research</p>
+                                <p>User Experience (UX) Design</p>
+                                <p>User Interface (UI) Design</p>
+                            </Col>
+                            <Col className="col-offer"><p>Prototyping and Testing</p>
+                                <p>Web UX/UI Design</p>
+                                <p>Mobile App UX/UI Design</p>
+                            </Col>
+                        </Row>
+                    </div>
                 )}
                 {selectedText === "Text 3" && (
-                   <div className="specific-div">
-                   <Image src='' width={'880px'} style={{borderRadius:'24px'}}/>
-                   <Row>
-                 
-                    <Col className="col-offer"><p>Logo Design</p>
-                    <p>Video Creation & Editing</p>
-                    <p>Business Card Design</p>
-                 </Col>
-                    <Col className="col-offer"><p>Banner and Signage Design</p>
-                    <p>Display Advertising</p>
-                    <p>Product Labelling</p>
-                  </Col>
-                   </Row>
-                </div>
+                    <div className="specific-div">
+                        <Image src='' width={'880px'} style={{ borderRadius: '24px' }} />
+                        <Row className="TextToggle-row">
+
+                            <Col className="col-offer"><p>Logo Design</p>
+                                <p>Video Creation & Editing</p>
+                                <p>Business Card Design</p>
+                            </Col>
+                            <Col className="col-offer"><p>Banner and Signage Design</p>
+                                <p>Display Advertising</p>
+                                <p>Product Labelling</p>
+                            </Col>
+                        </Row>
+                    </div>
                 )}
                 {selectedText === "Text 4" && (
-                   <div className="specific-div">
-                   <Image src='' width={'880px'} style={{borderRadius:'24px'}}/>
-                   <Row>
-                 
-                    <Col className="col-offer"><p>Logo Design</p>
-                    <p>Video Creation & Editing</p>
-                    <p>Business Card Design</p>
-                    <p>Packaging Design</p>
-                    <p>Branding and Identity Design</p>
-                    <p>Infographics</p></Col>
-                    <Col className="col-offer"><p>Banner and Signage Design</p>
-                    <p>Display Advertising</p>
-                    <p>Product Labelling</p>
-                    <p>Social Media Post and Ad Design</p>
-                    <p>Print Design (Brochures & Flyers) /</p>
-                    <p>Social Media Posters /</p></Col>
-                   </Row>
-                </div>
+                    <div className="specific-div">
+                        <Image src='' width={'880px'} style={{ borderRadius: '24px' }} />
+                        <Row className="TextToggle-row">
+
+                            <Col className="col-offer"><p>Logo Design</p>
+                                <p>Video Creation & Editing</p>
+                                <p>Business Card Design</p>
+                                <p>Packaging Design</p>
+                                <p>Branding and Identity Design</p>
+                                <p>Infographics</p></Col>
+                            <Col className="col-offer"><p>Banner and Signage Design</p>
+                                <p>Display Advertising</p>
+                                <p>Product Labelling</p>
+                                <p>Social Media Post and Ad Design</p>
+                                <p>Print Design (Brochures & Flyers) /</p>
+                                <p>Social Media Posters /</p></Col>
+                        </Row>
+                    </div>
                 )}
                 {selectedText === "Text 5" && (
-                   <div className="specific-div">
-                   <Image src='' width={'880px'} style={{borderRadius:'24px'}}/>
-                   <Row>
-                 
-                    <Col className="col-offer"><p>Logo Design</p>
-                    <p>Video Creation & Editing</p>
-                    <p>Business Card Design</p>
-                    <p>Packaging Design</p>
-                    <p>Branding and Identity Design</p>
-                    <p>Infographics</p></Col>
-                    <Col className="col-offer"><p>Banner and Signage Design</p>
-                    <p>Display Advertising</p>
-                    <p>Product Labelling</p>
-                    <p>Social Media Post and Ad Design</p>
-                    <p>Print Design (Brochures & Flyers) /</p>
-                    <p>Social Media Posters /</p></Col>
-                   </Row>
-                </div>
+                    <div className="specific-div">
+                        <Image src='' width={'880px'} style={{ borderRadius: '24px' }} />
+                        <Row className="TextToggle-row">
+
+                            <Col className="col-offer"><p>Logo Design</p>
+                                <p>Video Creation & Editing</p>
+                                <p>Business Card Design</p>
+                                <p>Packaging Design</p>
+                                <p>Branding and Identity Design</p>
+                                <p>Infographics</p></Col>
+                            <Col className="col-offer"><p>Banner and Signage Design</p>
+                                <p>Display Advertising</p>
+                                <p>Product Labelling</p>
+                                <p>Social Media Post and Ad Design</p>
+                                <p>Print Design (Brochures & Flyers) /</p>
+                                <p>Social Media Posters /</p></Col>
+                        </Row>
+                    </div>
                 )}
             </div>
         </div>
